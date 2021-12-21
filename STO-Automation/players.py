c6b82425162b82259b13445c56ab2ad2e4e7044a
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2021-12-21 22:23:03 +0200

    Update players.py

--- STO-Automation/players.py
+++ STO-Automation/players.py
@@ -42,20 +42,21 @@ class Player1:
         # Duty Officers vars
         self.duff_folder_x, self.duff_folder_y = 410, 20
         self.completed_x, self.completed_y = 85, 235
-        self.filters_x, self.filters_y = 880, 50
-        self.metReqs_x, self.metReqs_y = 862, 99
-        self.personal_x, self.personal_y = 70, 130
-        self.department_x, self.department_y = 90, 165
-        self.engineering_x, self.engineering_y = 485, 360
-        self.operations_x, self.operations_y = 835, 360
-        self.science_x, self.science_y = 485, 465
-        self.science_plan_x, self.science_plan_y = 965, 265
-        self.medial_x, self.medical_y = 835, 465
-        self.tactical_x, self.tactical_y = 485, 570
-        self.security_x, self.security_y = 835, 570
         self.duff_1_x, self.duff_1_y = 935, 175
-        self.plan_x, self.plan_y = 965, 255
-        self.begin_x, self.begin_y = 955, 1025
+
+        # self.filters_x, self.filters_y = 880, 50
+        # self.metReqs_x, self.metReqs_y = 862, 99
+        # self.personal_x, self.personal_y = 70, 130
+        # self.department_x, self.department_y = 90, 165
+        # self.engineering_x, self.engineering_y = 485, 360
+        # self.operations_x, self.operations_y = 835, 360
+        # self.science_x, self.science_y = 485, 465
+        # self.science_plan_x, self.science_plan_y = 965, 265
+        # self.medial_x, self.medical_y = 835, 465
+        # self.tactical_x, self.tactical_y = 485, 570
+        # self.security_x, self.security_y = 835, 570
+        # self.plan_x, self.plan_y = 965, 255
+        # self.begin_x, self.begin_y = 955, 1025
 
     # Run Reputation Automation
     def act_reputation(self):
@@ -234,7 +235,7 @@ class Player1:
                 logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', gmtime())} : "
                              f"Player 1: Collecting reward #{i}...\n")
                 click(self.duff_1_x, self.duff_1_y)
-                time.sleep(0.6)
+                time.sleep(0.4)
                 print(f"[i]Player 1: reward #{i} collected.")
                 logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', gmtime())} : "
                              f"Player 1: Collected Reward #{i}.\n")
@@ -252,31 +253,31 @@ class Player1:
             time.sleep(pause)
 
             # Click on Personal
-            pyautogui.moveTo(self.personal_x, self.personal_y, duration=dur)
+            pyautogui.moveTo(personal_x, personal_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Clicking on Personal")
-            click(self.personal_x, self.personal_y)
+            click(personal_x, personal_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Personal.\n")
             time.sleep(duff_pause)
 
             # Click on Filters
-            pyautogui.moveTo(self.filters_x, self.filters_y, duration=dur)
+            pyautogui.moveTo(filters_x, filters_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Personal: Clicking Filters")
-            click(self.filters_x, self.filters_y)
+            click(filters_x, filters_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Filters.\n")
             time.sleep(duff_pause)
 
             # Click on Met Reqs
-            pyautogui.moveTo(self.metReqs_x, self.metReqs_y, duration=dur)
+            pyautogui.moveTo(metReqs_x, metReqs_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Personal: Clicking on Met Reqs")
-            click(self.metReqs_x, self.metReqs_y)
+            click(metReqs_x, metReqs_y)
             time.sleep(duff_pause)
-            click(self.metReqs_x, self.metReqs_y)
-            pyautogui.doubleClick(self.metReqs_x, self.metReqs_y, button='left')
+            click(metReqs_x, metReqs_y)
+            pyautogui.doubleClick(metReqs_x, metReqs_y, button='left')
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Met Reqs.\n")
             time.sleep(duff_pause)
@@ -285,10 +286,10 @@ class Player1:
             self.planExecute()
 
             # Click on Engineering Assignments
-            pyautogui.moveTo(self.engineering_x, self.engineering_y, duration=dur)
+            pyautogui.moveTo(engineering_x, engineering_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Clicking on Engineering")
-            click(self.engineering_x, self.engineering_y)
+            click(engineering_x, engineering_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Engineering.\n")
             time.sleep(duff_pause)
@@ -297,10 +298,10 @@ class Player1:
             self.planExecute()
 
             # Click on Science Assignments
-            pyautogui.moveTo(self.science_x, self.science_y, duration=dur)
+            pyautogui.moveTo(science_x, science_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Science Assignment")
-            click(self.science_x, self.science_y)
+            click(science_x, science_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Science Assignments.\n")
             time.sleep(duff_pause)
@@ -309,10 +310,10 @@ class Player1:
             self.planExecute()
 
             # Click on Tactical Assignments
-            pyautogui.moveTo(self.tactical_x, self.tactical_y, duration=dur)
+            pyautogui.moveTo(tactical_x, tactical_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Tactical Assignment")
-            click(self.tactical_x, self.tactical_y)
+            click(tactical_x, tactical_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Tactical Assignments.\n")
             time.sleep(duff_pause)
@@ -321,10 +322,10 @@ class Player1:
             self.planExecute()
 
             # Click on Security Assignments
-            pyautogui.moveTo(self.security_x, self.security_y, duration=dur)
+            pyautogui.moveTo(security_x, security_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Security Assignment")
-            click(self.security_x, self.security_y)
+            click(security_x, security_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Security Assignments.\n")
             time.sleep(duff_pause)
@@ -333,10 +334,10 @@ class Player1:
             self.planExecute()
 
             # Click on Medical Assignments
-            pyautogui.moveTo(self.medial_x, self.medical_y, duration=dur)
+            pyautogui.moveTo(medial_x, medical_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Medical Assignment")
-            click(self.medial_x, self.medical_y)
+            click(medial_x, medical_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Medical Assignments.\n")
             time.sleep(duff_pause)
@@ -364,10 +365,10 @@ class Player1:
                 self.begin()
 
             # Return to Department Heads
-            pyautogui.moveTo(self.department_x, self.department_y, duration=dur)
+            pyautogui.moveTo(department_x, department_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Department Heads")
-            click(self.department_x, self.department_y)
+            click(department_x, department_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Department Heads.\n")
             time.sleep(duff_pause)
@@ -377,9 +378,9 @@ class Player1:
     # General Plan
     def plan(self):
         with open(log, 'a+') as logger:
-            pyautogui.moveTo(self.plan_x, self.plan_y, duration=dur)
+            pyautogui.moveTo(plan_x, plan_y, duration=dur)
             time.sleep(pause)
-            click(self.plan_x, self.plan_y)
+            click(plan_x, plan_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Plan.\n")
             time.sleep(pause)
@@ -389,9 +390,9 @@ class Player1:
     # General Begin Assignment
     def begin(self):
         with open(log, 'a+') as logger:
-            pyautogui.moveTo(self.begin_x, self.begin_y, duration=dur)
+            pyautogui.moveTo(begin_x, begin_y, duration=dur)
             time.sleep(pause)
-            click(self.begin_x, self.begin_y)
+            click(begin_x, begin_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Begin Assignment.\n")
             time.sleep(duff_pause)
@@ -434,21 +435,6 @@ class Player2:
         self.duff_folder_x, self.duff_folder_y = 410, 20
         self.completed_x, self.completed_y = 85, 235
         self.duff_1_x, self.duff_1_y = 935, 175
-
-        # Setup Assignments Coordinates
-        self.department_x, self.department_y = 90, 165
-        self.filters_x, self.filters_y = 880, 50
-        self.metReqs_x, self.metReqs_y = 862, 99
-        self.personal_x, self.personal_y = 70, 130
-        self.engineering_x, self.engineering_y = 485, 360
-        self.operations_x, self.operations_y = 835, 360
-        self.science_x, self.science_y = 485, 465
-        self.science_plan_x, self.science_plan_y = 965, 265
-        self.medial_x, self.medical_y = 835, 465
-        self.tactical_x, self.tactical_y = 485, 570
-        self.security_x, self.security_y = 835, 570
-        self.plan_x, self.plan_y = 965, 255
-        self.begin_x, self.begin_y = 955, 1025
 
     # Run Reputation Automation
     def act_reputation(self):
@@ -642,7 +628,7 @@ class Player2:
                 print(f"[i]Player 2: Collecting reward #{i}")
                 logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', gmtime())} : Player 2: Collecting reward.\n")
                 click(self.duff_1_x, self.duff_1_y)
-                time.sleep(1)
+                time.sleep(0.4)
                 print(f"[i]Player 2 : reward #{i} collected")
                 logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', gmtime())} : Player 2: Reward collected.\n")
 
@@ -659,43 +645,46 @@ class Player2:
             time.sleep(pause)
 
             # Click on Personal
-            pyautogui.moveTo(self.personal_x, self.personal_y, duration=dur)
+            pyautogui.moveTo(personal_x, personal_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Clicking on Personal")
-            click(self.personal_x, self.personal_y)
+            click(personal_x, personal_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Personal.\n")
             time.sleep(duff_pause)
 
             # Click on Filters
-            pyautogui.moveTo(self.filters_x, self.filters_y, duration=dur)
+            pyautogui.moveTo(filters_x, filters_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Personal: Clicking Filters")
-            click(self.filters_x, self.filters_y)
+            click(filters_x, filters_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Filters.\n")
             time.sleep(duff_pause)
 
             # Click on Met Reqs
-            pyautogui.moveTo(self.metReqs_x, self.metReqs_y, duration=dur)
+            pyautogui.moveTo(metReqs_x, metReqs_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Personal: Clicking on Met Reqs")
-            click(self.metReqs_x, self.metReqs_y)
+            click(metReqs_x, metReqs_y)
             time.sleep(duff_pause)
-            click(self.metReqs_x, self.metReqs_y)
-            pyautogui.doubleClick(self.metReqs_x, self.metReqs_y, button='left')
+            click(metReqs_x, metReqs_y)
+            pyautogui.doubleClick(metReqs_x, metReqs_y, button='left')
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Met Reqs.\n")
+
+            # Clear Mouse Status
+            win32api.mouse_event(win32con.MOUSEEVENTF_ABSOLUTE, metReqs_x, metReqs_y, 0, 0)
             time.sleep(duff_pause)
 
             # Start Planning & Executing for Personal Assignments
             self.planExecute()
 
             # Click on Engineering Assignments
-            pyautogui.moveTo(self.engineering_x, self.engineering_y, duration=dur)
+            pyautogui.moveTo(engineering_x, engineering_y, duration=dur)
             time.sleep(pause)
             print(f"[i]Player 1: Clicking on Engineering")
-            click(self.engineering_x, self.engineering_y)
+            click(engineering_x, engineering_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Engineering.\n")
             time.sleep(duff_pause)
@@ -704,10 +693,10 @@ class Player2:
             self.planExecute()
 
             # Click on Science Assignments
-            pyautogui.moveTo(self.science_x, self.science_y, duration=dur)
+            pyautogui.moveTo(science_x, science_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Science Assignment")
-            click(self.science_x, self.science_y)
+            click(science_x, science_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Science Assignments.\n")
             time.sleep(duff_pause)
@@ -716,10 +705,10 @@ class Player2:
             self.planExecute()
 
             # Click on Tactical Assignments
-            pyautogui.moveTo(self.tactical_x, self.tactical_y, duration=dur)
+            pyautogui.moveTo(tactical_x, tactical_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Tactical Assignment")
-            click(self.tactical_x, self.tactical_y)
+            click(tactical_x, tactical_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Tactical Assignments.\n")
             time.sleep(duff_pause)
@@ -728,10 +717,10 @@ class Player2:
             self.planExecute()
 
             # Click on Security Assignments
-            pyautogui.moveTo(self.security_x, self.security_y, duration=dur)
+            pyautogui.moveTo(security_x, security_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Security Assignment")
-            click(self.security_x, self.security_y)
+            click(security_x, security_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Security Assignments.\n")
             time.sleep(duff_pause)
@@ -740,10 +729,10 @@ class Player2:
             self.planExecute()
 
             # Click on Medical Assignments
-            pyautogui.moveTo(self.medial_x, self.medical_y, duration=dur)
+            pyautogui.moveTo(medial_x, medical_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Medical Assignment")
-            click(self.medial_x, self.medical_y)
+            click(medial_x, medical_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Medical Assignments.\n")
             time.sleep(duff_pause)
@@ -771,10 +760,10 @@ class Player2:
                 self.begin()
 
             # Return to Department Heads
-            pyautogui.moveTo(self.department_x, self.department_y, duration=dur)
+            pyautogui.moveTo(department_x, department_y, duration=dur)
             time.sleep(pause)
             print("[i]Clicking on Department Heads")
-            click(self.department_x, self.department_y)
+            click(department_x, department_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Department Heads.\n")
             time.sleep(duff_pause)
@@ -784,21 +773,21 @@ class Player2:
     # General Plan
     def plan(self):
         with open(log, 'a+') as logger:
-            pyautogui.moveTo(self.plan_x, self.plan_y, duration=dur)
+            pyautogui.moveTo(plan_x, plan_y, duration=dur)
             time.sleep(pause)
-            click(self.plan_x, self.plan_y)
+            click(plan_x, plan_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Plan.\n")
-            time.sleep(pause)
+            # time.sleep(pause)
 
         return
 
     # General Begin Assignment
     def begin(self):
         with open(log, 'a+') as logger:
-            pyautogui.moveTo(self.begin_x, self.begin_y, duration=dur)
+            pyautogui.moveTo(begin_x, begin_y, duration=dur)
             time.sleep(pause)
-            click(self.begin_x, self.begin_y)
+            click(begin_x, begin_y)
             logger.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} : "
                          f"Player 1: Clicked on Begin Assignment.\n")
             time.sleep(duff_pause)
@@ -820,6 +809,21 @@ def click(x, y):
     # Clear Mouse Status
     win32api.mouse_event(win32con.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0)
 
+
+# Setup Assignments Coordinates
+department_x, department_y = 90, 165
+filters_x, filters_y = 880, 50
+metReqs_x, metReqs_y = 862, 99
+personal_x, personal_y = 70, 130
+engineering_x, engineering_y = 485, 360
+operations_x, operations_y = 835, 360
+science_x, science_y = 485, 465
+science_plan_x, science_plan_y = 965, 265
+medial_x, medical_y = 835, 465
+tactical_x, tactical_y = 485, 570
+security_x, security_y = 835, 570
+plan_x, plan_y = 965, 255
+begin_x, begin_y = 955, 1025
 
 duff_pause = 1.5
 pause = 0.2
