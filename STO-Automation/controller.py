e53c971d88855ba19eaf0f2a14aa5a2d50798d8c
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2022-02-25 06:01:28 +0200

    Update controller.py

--- STO-Automation/controller.py
+++ STO-Automation/controller.py
@@ -469,21 +469,6 @@ def logIt(logfile, write=True, debug=False, msg=''):
             lf.write(f"{datetime.today().replace(microsecond=0)}: {msg}\n")
 
 
-def clickBAK(x, y):
-    # Clear Mouse Status
-    win32api.mouse_event(win32con.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0)
-
-    # Set Mouse Position
-    win32api.SetCursorPos((x, y))
-
-    # Commence Mouse Click
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
-    # Clear Mouse Status
-    win32api.mouse_event(win32con.MOUSEEVENTF_ABSOLUTE, x, y, 0, 0)
-
-
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
