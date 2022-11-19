784e88fab6a983a6cdb317bfc0403f77a713dbf5
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2022-11-19 17:10:06 +0200

    Add files via upload

--- Peach/Client/Setup/setup.ps1
+++ Peach/Client/Setup/setup.ps1
@@ -17,108 +17,24 @@
     }
 }
 
-function Download-Files($dst_path) {
-    # Github files links
-    $clientEXE_url = "https://www28.zippyshare.com/d/To8yIHCh/41300/client.exe"
-    $clientEXE_outpath = "$dst_path\client.exe"
-
-    $clientPNG_url = "https://github.com/GShwartz/Python/raw/main/Peach/Client/Setup/client.png"
-    $clientPNG_outpath = "$dst_path\client.png"
-
-    $clientBAT_url = "https://raw.githubusercontent.com/GShwartz/Python/main/Peach/Client/Setup/client.bat"
-    $clientBAT_outpath = "$dst_path\client.bat"
-
-    $clientVBS_url = "https://raw.githubusercontent.com/GShwartz/Python/main/Peach/Client/Setup/client.vbs"
-    $clientVBS_outpath = "$dst_path\client.vbs"
-
-    $updaterEXE_url = "https://github.com/GShwartz/Python/raw/main/Peach/Client/Setup/updater.exe"
-    $updaterEXE_outpath = "$dst_path\updater.exe"
-
-    $requirements_url = "https://raw.githubusercontent.com/GShwartz/Python/main/Peach/Client/Setup/requirements.txt"
-    $requirements_outpath = "$dst_path\requirements.txt"
-
-    # client.exe
-    if (-not(Test-Path -Path $clientEXE_outpath -PathType Leaf)) {
-        try {
-            Write-Host "Downloading client.exe..."
-            $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($clientEXE_url, $clientEXE_outpath)
-    
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-
-    # client.png
-    if (-not(Test-Path -Path $clientPNG_outpath -PathType Leaf)) {
-        try {
-            Write-Host "Downloading client.png..."
-            $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($clientPNG_url, $clientPNG_outpath)
-    
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-
-    # client.bat
-    if (-not(Test-Path -Path $clientBAT_outpath -PathType Leaf)) {
-        try {
-            Write-Host "Downloading client.bat..."
-            $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($clientBAT_url, $clientBAT_outpath)
-    
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-
-    # client.vbs
-    if (-not(Test-Path -Path $clientVBS_outpath -PathType Leaf)) {
-        try {
-            Write-Host "Downloading client.vbs..."
-            $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($clientVBS_url, $clientVBS_outpath)
-    
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-
-    # updater.exe
-    if (-not(Test-Path -Path $updaterEXE_outpath -PathType Leaf)) {
-        try {
-            Write-Host "Downloading updater.exe..."
-            $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($updaterEXE_url, $updaterEXE_outpath)
-    
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-
-    # requirements.txt
-    if (-not(Test-Path -Path $requirements_outpath -PathType Leaf)) {
-        try {
-            Write-Host "Downloading requirements.txt..."
-            $wc = New-Object System.Net.WebClient
-            $wc.DownloadFile($requirements_url, $requirements_outpath)
-    
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-
-}
-
 # Destination path
 $dst_path = 'C:\HandsOff'
+$clientEXE = '.\client.exe'
+$clientBAT = '.\client.bat'
+$clientVBS = '.\client.vbs'
+$clientPNG = '.\client.png'
+$clientICO = '.\client.ico'
+$updaterEXE = '.\updater.exe'
+$requirementsTXT = '.\requirements.txt'
+
+$clientEXE_outpath = "$dst_path\client.exe"
+$clientBAT_outpath = "$dst_path\client.bat"
+$clientPNG_outpath = "$dst_path\client.png"
+$clientVBS_outpath = "$dst_path\client.vbs"
+$clientICO_outpath = "$dst_path\client.ico"
+$updaterEXE_outpath = "$dst_path\updater.exe"
+$requirements_outpath = "$dst_path\requirements.txt"
+
 
 # Python Vars
 $python_version = "3.10.8"
@@ -140,8 +56,14 @@ New-Item -ItemType Directory -Force -Path "C:\HandsOff"
 # Install python
 Install-Python $python_version $python_url $python_outpath
 
-# Download client files
-Download-Files $dst_path
+# Copy files
+Copy-Item $clientEXE -Destination '$clientEXE_outpath' -Force
+Copy-Item $clientBAT -Destination '$clientBAT_outpath' -Force
+Copy-Item $clientVBS -Destination '$clientVBS_outpath' -Force
+Copy-Item $clientPNG -Destination '$clientPNG_outpath' -Force
+Copy-Item $clientICO -Destination '$clientICO_outpath' -Force
+Copy-Item $updaterEXE -Destination '$updaterEXE_outpath' -Force
+Copy-Item $requirements -Destination '$requirements_outpath' -Force
 
 # Install python requirements
 pip install -r c:\HandsOff\requirements.txt
@@ -156,7 +78,7 @@ Write-Host "Done!"
 
 # Set Autorun in Registry
 Write-Host "Setting up persistence..."
-$persistence = Set-Itemproperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'Peach' -Value 'c:\HandsOff\client.bat' -Force
+$persistence = Set-Itemproperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'HandsOff' -Value 'c:\HandsOff\client.bat' -Force
 Write-Host "Done."
 
 # Open Port 55400 For Incoming & Outgoing Traffic
